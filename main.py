b027b54f718947e057de78454ce7f1d2cf585be7
Sungbum Jun <jun23@purdue.edu>
2020-02-03 22:00:27 -0500

    Decision Tree Testing

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
 from sklearn.datasets import load_iris
-from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
+from sklearn import tree
 from sklearn.externals.six import StringIO
-from sklearn.tree import export_graphviz
 import numpy as np
 import pandas as pd
 import graphviz
 
-
-# This code is origianlly acquired from https://towardsdatascience.com/decision-tree-in-python-b433ae57fb93
-def decision_tree():
-    iris = load_iris()
-    X = pd.DataFrame(iris.data, columns=iris.feature_names)
-    y = pd.Categorical.from_codes(iris.target, iris.target_names)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)
-    dt = DecisionTreeClassifier()
-    dt.fit(X_train, y_train)
-    dot_data = DecisionTreeClassifier.export_graphviz(dt, out_file=None)
-    graph = graphviz.Source(dot_data)
-    graph.render("iris")
+# This code is originally acquired from https://towardsdatascience.com/decision-tree-in-python-b433ae57fb93
 
 
 def main():
+    iris = load_iris()
+    X = pd.DataFrame(iris.data, columns=iris.feature_names)
+    y = pd.Categorical.from_codes(iris.target, iris.target_names)
+    # X.head()
+    y = pd.get_dummies(y)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)
+    dt = tree.DecisionTreeClassifier()
+    dt.fit(X_train, y_train)
+    dot_data = tree.export_graphviz(dt, out_file=None, feature_names=iris.feature_names, class_names=iris.target_names,
+                                    filled=True, rounded=True, special_characters=True)
+    graph = graphviz.Source(dot_data)
+    graph
     print("End of Program")
 
 
